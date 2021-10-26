f095bf861c01148b77e4d222d4452bb8c375e5f6
Máté Csaba Sándor <sampaat@gmail.com>
2021-10-26 17:32:26 +0200

    Update data_preparation_bitcoin.R
    Updated script used to create dataset version 1.1.0

--- data_preparation_bitcoin.R
+++ data_preparation_bitcoin.R
@@ -50,7 +50,8 @@ gamesTable <- c(
 # eol characters may have to be changed dependent on windows/unix systems used
 
 #get addrIDs connected to the adresses in the game table
-satoshi_addrIDs <- read.csv.sql(file = "bitcoin_2013_addresses.dat",
+#use bitcoin_2013_addresses.csv.gz
+satoshi_addrIDs <- read.csv.sql(file = "rawdata/kondor_to_14/addresses.txt",
                                 header = F,
                                 eol = "\n",
                                 sep = "\t",
@@ -64,7 +65,8 @@ satoshi_addrIDs <- read.csv.sql(file = "bitcoin_2013_addresses.dat",
 #bets placed with timestamps
 
 #get bets placed on the game adresses
-bet_placed_tx <- read.csv.sql(file = "bitcoin_2013_txout.txt",
+#use bitcoin_2013_txout.csv.xz
+bet_placed_tx <- read.csv.sql(file = "rawdata/kondor_to_14/txout.txt",
                               header = F,
                               eol = "\n",
                               sep = "\t",
@@ -74,41 +76,27 @@ bet_placed_tx <- read.csv.sql(file = "bitcoin_2013_txout.txt",
   select(V1, V2, V3) %>%
   rename(txID = V1, addrID = V2, value = V3)
 
-#adding block id
-bet_placed_blocked_tx <- read.csv.sql("bitcoin_2013_tx.csv",
-                                      header = F, 
-                                      eol = "\n",
-                                      sep = "\t", 
-                                      sql = "select * from bet_placed_tx
-                                         inner join file on bet_placed_tx.txID = file.v1"
-) %>% 
-  rename(blockID = V2) %>%
-  select(-V1) %>%
-  select(-V3) %>%
-  select(-V4)
-
 #adding timestamps
-bet_placed_timed_tx <- read.csv.sql("bitcoin_2013_blockhash.csv",
-                                    header = F, 
+bet_placed_timed_tx <- read.csv.sql("rawdata/kondor_to_14/txtime.txt",
+                                    header = F,
                                     eol = "\n",
-                                    sep = "\t", 
-                                    sql = "select * from bet_placed_blocked_tx
-                                         inner join file on bet_placed_blocked_tx.blockID = file.v1"
-) %>% 
-  rename(time = V3) %>%
-  select(-V1) %>%
-  select(-V2) %>%
-  select(-V4)
+                                    sep = "\t",
+                                    sql = "select * from bet_placed_tx
+                                         inner join file on bet_placed_tx.txID = file.v1"
+) %>%
+  rename(time = V2) %>%
+  select(-V1)
+
 
 # delete big variables of intermittent steps
-rm(bet_placed_blocked_tx, bet_placed_tx)
+rm(bet_placed_tx)
 gc()
 
 ##########################
 #bets answers by satoshidce with timestamps
 
 #get answers sent by game addresses
-bet_answered_tx <- read.csv.sql("bitcoin_2013_txin.txt",
+bet_answered_tx <- read.csv.sql("rawdata/kondor_to_14/txin.txt",
                                 header = F, 
                                 eol = "\n",
                                 sep = "\t", 
@@ -117,35 +105,19 @@ bet_answered_tx <- read.csv.sql("bitcoin_2013_txin.txt",
 ) %>% 
   select(V1, V2, V3) %>% 
   rename(txID = V1, addrID = V2, value = V3)
-
-#adding block id
-bet_answered_blocked_tx <- read.csv.sql("bitcoin_2013_tx.csv",
+#adding transaction time
+bet_answered_timed_tx <- read.csv.sql("rawdata/kondor_to_14/txtime.txt",
                                       header = F, 
                                       eol = "\n",
                                       sep = "\t", 
-                                      sql = "select * from bet_placed_tx
-                                         inner join file on bet_placed_tx.txID = file.v1"
+                                      sql = "select * from bet_answered_tx
+                                         inner join file on bet_answered_tx.txID = file.v1"
 ) %>% 
-  rename(blockID = V2) %>%
-  select(-V1) %>%
-  select(-V3) %>%
-  select(-V4)
-
-#adding timestamps
-bet_answered_timed_tx <- read.csv.sql("bitcoin_2013_blockhash.csv",
-                                    header = F, 
-                                    eol = "\n",
-                                    sep = "\t", 
-                                    sql = "select * from bet_placed_blocked_tx
-                                         inner join file on bet_placed_blocked_tx.blockID = file.v1"
-) %>% 
-  rename(time = V3) %>%
-  select(-V1) %>%
-  select(-V2) %>%
-  select(-V4)
+  rename(time = V2) %>%
+  select(-V1)
 
 # delete big variables of intermittent steps
-rm(bet_answered_tx, bet_answered_blocked_tx)
+rm(bet_answered_tx)
 gc()
 
 ##########################
@@ -167,26 +139,28 @@ bet_placed_timed_tx %>%
 source("bitcoin_pricehistory_lowvar.R")
 
 #the calculation below is quite memory intensive, its suggested to do 1 at a time and then increase the WINDOW variable between on c(1,2,3,4,5)
-WINDOW <- 1
+WINDOW <- 5
 
+#filter for the sample timeframe
 bet_placed_timed_tx_win1 <- bet_placed_timed_tx %>%
   filter((time > ranges_selected_data$timestamp_start[[WINDOW]])&(time < ranges_selected_data$timestamp_end[[WINDOW]]))
 bet_answered_timed_tx_win1 <- bet_answered_timed_tx %>%
   filter((time > ranges_selected_data$timestamp_start[[WINDOW]])&(time < ranges_selected_data$timestamp_end[[WINDOW]]))
 
 #adding incoming transaction of bets
-bet_placed_timed_tx_win1_in <- read.csv.sql("bitcoin_2013_txin.txt",
+bet_placed_timed_tx_win1_in <- read.csv.sql("rawdata/kondor_to_14/txin.txt",
                                             header = F, 
                                             eol = "\n",
                                             sep = "\t", 
                                             sql = "select * from bet_placed_timed_tx_win1
                                    left join file on bet_placed_timed_tx_win1.txID = file.v1"
 ) %>% 
-  rename(addrID_in = V2, value_in = V3) %>%
-  select(-V1)
+  rename(addrID_in = V2) %>%
+  select(-V1) %>%
+  select(-V3)
 
 #adding target addresses of answers
-bet_answered_timed_tx_win1_out <- read.csv.sql("bitcoin_2013_txout.txt",
+bet_answered_timed_tx_win1_out <- read.csv.sql("rawdata/kondor_to_14/txout.txt",
                                                header = F, 
                                                eol = "\n",
                                                sep = "\t", 
@@ -196,32 +170,84 @@ bet_answered_timed_tx_win1_out <- read.csv.sql("bitcoin_2013_txout.txt",
   rename(addrID_out = V2, value_out = V3) %>%
   select(-V1)
 
-#we are adding winning probabilies to te game from the data scraped from the historycal webpage
-win_odds <- gamesTable %>% 
-  inner_join(satoshi_addrIDs, by ="Address") %>%
-  select(addrID_game = addrID, PriceMultiplier)
-
 #we are pairing up all bets with all possible answer transactions
 bet_match_win1 <- bet_placed_timed_tx_win1_in %>% 
   left_join(bet_answered_timed_tx_win1_out, by = c("addrID_in" = "addrID_out", "addrID", "value"))
 
+rm(bet_placed_timed_tx_win1, bet_answered_timed_tx_win1, bet_answered_timed_tx_win1_out)
+gc()
+
 #basic method with just selecting the match with the smallest positive time diff 
 #this can produce false matches: bets made in close succession can be paired with the same answer, but we can test for duplications after
-bet_match_finallized <- bet_match_win1 %>%
+
+FEE = ifelse(WINDOW < 2,0,50000) #after 2012.oct 11 there is a transaction fee included
+
+txy_used <- c()
+pairReturnTransaction <- function(x,...){
+#this function makes sure we dont use the same return transaction twice
+  line_match <- x %>% 
+    filter(!(txID.y %in% txy_used)) %>%
+    top_n(n = 1, wt = desc(timediff))
+    
+    txy_used <<- c(txy_used,line_match$txID.y) 
+    return(line_match)
+}
+bet_match_uniqued <- bet_match_win1 %>%
   drop_na() %>%
   mutate(timediff = time.y - time.x) %>%
-  filter(timediff >= 0) %>% #answers startet before the bet are obviously dropped
+  filter(timediff >= 0)  %>%
+  arrange(time.x) %>%
   group_by(txID.x, addrID, value) %>%
-  top_n(n = 1, wt = desc(timediff)) %>% #we are pairing bets up with the first return transaction to the bettor address
+  group_modify(pairReturnTransaction)
+txy_used <- c()
+
+bet_match_finallized <- bet_match_uniqued %>%
   select(txID.x, txID.y, time.x, timediff, addrID_in, addrID, value, value_out) %>%
   distinct() %>%
   left_join(win_odds, by = c("addrID" = "addrID_game")) %>%
-  mutate(ratt = ifelse(WINDOW < 3,(value_out)/value, (value_out+50000)/value)) %>% #after 2012.oct 11 there is a transaction fee included
-  mutate(status = ifelse(ratt > .9,"win","loose")) #win/loss flag is calculated based on the return transaction ammount (for small bet ammounts with minimal risk games the answer can be still below of the bet ammount cause of the transaction cost)
+  mutate(ratt = (value_out+FEE)/value) %>%
+  mutate(status = ifelse(ratt > .9,"win","loss")) %>% #win/loss flag is calculated based on the return transaction amount
+  mutate(status = ifelse(value == value_out, NA, status)) %>% #when the bet is above the maximum or below the minimum amount, the answer transaction is identical in amount
+  arrange(txID.x, addrID, value)
+
+#count bets where we couldn't decide the pairs (idiosyncratic answers)
+#78 transactions found for WINDOW 1 (444-2/1241-3/4380-4/1344-5)
+bet_match_finallized %>%
+  group_by(txID.x, addrID, value) %>%
+  filter(n()>1) %>%
+  ungroup() %>%
+  pull(txID.x) %>%
+  unique() %>%
+  length()
+#lets also check if we have used any answer multiple times (should be zero)
+bet_match_finallized %>%
+  group_by(txID.y, addrID, value) %>%
+  filter(n()>1) %>%
+  ungroup() %>%
+  pull(txID.y) %>%
+  unique() %>%
+  length()
+
+#lets just add NA status to them
+bet_match_finallized_idiosinc <- bet_match_finallized %>%
+  group_by(txID.x, addrID, value) %>%
+  filter(n()>1) %>%
+  top_n(n = 1) %>%
+  ungroup() %>%
+  mutate(status = NA)
+
+#and replace them in the dataset
+
+bet_match_finallized <- bet_match_finallized %>%
+  group_by(txID.x, addrID, value) %>%
+  filter(n()==1) %>%
+  ungroup() %>%
+  bind_rows(bet_match_finallized_idiosinc)
 
 #we can check how many bets we have been able to match return transactions to
-#success rate really nice 96.82% for WINDOW 1
+#success rate really nice 95.77% for WINDOW 1 (94.59%-2/97.19%-3/94.65%-4/93.04%-5)
 bet_match_finallized %>% 
+  drop_na() %>%
   nrow() / {
     bet_placed_timed_tx_win1_in %>% 
       mutate(xxx = paste0(txID, addrID)) %>% 
@@ -229,11 +255,33 @@ bet_match_finallized %>%
       unique() %>% 
       length()}
 
+#check if the odds are about expected in the observations
+pwin_checkup <- bet_match_finallized %>% 
+  drop_na() %>%
+  mutate(win = status=="win") %>%
+  group_by(PriceMultiplier) %>%
+  summarize(n = n(),
+            win_rate = sum(win)/n()*100,
+            .groups = "drop") %>%
+  left_join(gamesTable %>% select(PriceMultiplier,WinOdds), by = "PriceMultiplier") %>%
+  mutate(pwin_diff = win_rate-WinOdds)  
+  
+#add users
+bet_match_finallized_usered <- read.csv.sql("rawdata/kondor_to_14/contraction2.csv",
+                                            header = T, 
+                                            eol = "\n",
+                                            sep = ",", 
+                                            sql = "select * from bet_match_finallized
+                              left join file on bet_match_finallized.addrID_in = file.addrID"
+)
+
 #prepare and save the results
 bet_match_finallized_merged_usered_tosave <- bet_match_finallized_usered[,-12] %>% 
   rename(txID_bet = txID.x, txID_answer = txID.y, time_bet = time.x, 
-         addrID_player = addrID_in, addrID_game = addrID, bet_value = value, answer_value = value_out) %>%
-  select(txID_bet, txID_answer, time_bet, timediff, addrID_player, addrID_game, bet_value, answer_value, PriceMultiplier, status, userID)
+         addrID_player = addrID_in, addrID_game = addrID, bet_value = value, answer_value = value_out, odds = PriceMultiplier) %>%
+  select(txID_bet, txID_answer, time_bet, timediff, addrID_player, addrID_game, bet_value, answer_value, odds, status, userID)
+
 
 #be sure to change the save files between windows so you do not overwrite your previous files
-write.csv(bet_match_finallized_merged_usered_tosave, file = "satoshidice_bets_matched.csv")
+write.csv(bet_match_finallized_merged_usered_tosave, file = paste("calcdata/satoshidice_bets_matched_",ranges_selected_data$start[[WINDOW]],"to",ranges_selected_data$end[[WINDOW]],"clean.csv",sep="_"))
+
